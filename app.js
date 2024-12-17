589e8f2d464d54e591797dc100ef1d680e642492
Sam Annetts <samannetts8@gmail.com>
2024-12-17 15:45:39 +0000

    Completed daily tasks

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ import {
   deleteQuote,
 } from "./quote.js";
 
+const quotes = await getQuotes();
+
 app.use(express.json());
 
 app.get("/", function (req, res) {
@@ -19,3 +21,49 @@ app.get("/", function (req, res) {
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
 });
+
+app.get("/quotes", (req,res) => {
+  res.send(quotes);
+})
+
+app.get("/quotes/:id", async (req,res) => {
+  const id = req.params.id;
+  try {
+  const specific_quote = await getQuoteByID(id);
+  res.send(specific_quote);
+  } catch (error) {
+    console.error('Error gaining specific quote:', error);
+  }
+})
+
+
+app.post('/quotes', async (req,res) =>{
+  try {
+    const {quoteText,author} = req.body;
+    const data = await addQuote(quoteText,author)
+    res.send(data);
+  } catch (error) {
+    console.error('Error editing an existing quote');
+  }
+})
+
+app.patch('/quotes/:id', async (req,res) =>{
+  try {
+    const id = req.params.id;
+    const {quoteText,author} = req.body;
+    const edited_quote = await editQuote(id,quoteText,author);
+    res.send(edited_quote);
+  } catch (error) {
+    console.error('Error editing an existing quote');
+  }
+}) 
+
+app.delete('/quotes/:id', async (req,res) =>{
+  try{
+    const id = req.params.id;
+    const removed_quote = await deleteQuote(id);
+    res.send(removed_quote);
+  } catch (error) {
+    console.error("Error deleting an existing quote")
+  }
+})
